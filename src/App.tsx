5832a404d1404fdc52419ba28cc3560ae2538a15
jkim000 <joshuakim514@gmail.com>
2023-05-25 00:38:33 -0700

    refactor: update App component with Guess components and mounting effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-function App() {
+import { observer, useLocalObservable } from "mobx-react-lite";
+import { useEffect } from "react";
+
+import Guess from "./components/Guess";
+import Keyboard from "./components/Keyboard";
+import WordleStore from "./store/WordleStore";
+
+const App = observer(() => {
+    const store = useLocalObservable(() => WordleStore);
+
+    useEffect(() => {
+        store.init();
+
+        window.addEventListener("keyup", store.handleKeyup);
+        return () => {
+            window.removeEventListener("keyup", store.handleKeyup);
+        };
+    }, [store]);
+
     return (
-        <>
-            <div>Wordle 6</div>
-        </>
+        <div className="flex flex-col justify-center h-screen w-screen">
+            <h1 className="m-2 text-center text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-700 to-orange-300">
+                6-Letter Wordle
+            </h1>
+            <h3 className="m-2 text-center text-xl">
+                To be impressive, your mode result should be 3 guess attempts
+            </h3>
+            <div className="my-4">
+                {new Array(6).fill(0).map((_, i) => (
+                    <Guess
+                        key={i}
+                        word={store.word}
+                        guess={store.guesses[i]}
+                        isGuessed={true}
+                    />
+                ))}
+            </div>
+            word: {store.word}
+            guesses: {JSON.stringify(store.guesses)}
+            <Keyboard />
+        </div>
     );
-}
+});
 
 export default App;
