9c219f9a82622fed175607f70d5e0180a0103072
jkim000 <joshuakim514@gmail.com>
2023-05-25 21:28:52 -0700

    feat: create Keyboard component

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,38 @@
+import { observer } from "mobx-react-lite";
+import { StoreProps } from "../types/types";
+
+interface KeyboardProps {
+    store: StoreProps;
+}
+
+const Keyboard = observer(({ store }: KeyboardProps) => {
+    const keyboardKeys = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+
+    return (
+        <>
+            {keyboardKeys.map((row, rowIndex) => (
+                <div key={rowIndex} className="flex justify-center">
+                    {row.split("").map((key, charIndex) => {
+                        const bgColor = store.exactGuesses.includes(key)
+                            ? "bg-green-600"
+                            : store.inexactGuesses.includes(key)
+                            ? "bg-yellow-500"
+                            : store.allGuesses.includes(key)
+                            ? "bg-gray-700"
+                            : "bg-gray-500";
+                        return (
+                            <div
+                                key={rowIndex + charIndex}
+                                className={`flex justify-center items-center m-1 h-12 w-12 rounded-md text-xl ${bgColor}`}
+                            >
+                                {key}
+                            </div>
+                        );
+                    })}
+                </div>
+            ))}
+        </>
+    );
+});
+
+export default Keyboard;
