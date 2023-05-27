f04d367021e795bd5fb64cfa7ee534801ca724ea
jkim000 <joshuakim514@gmail.com>
2023-05-26 18:08:51 -0700

    fix: duplicate letter guesses now correctly color as appropriate

--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -7,13 +7,11 @@ interface GuessProps {
 }
 
 export default function Guess({ word, guess, isGuessed }: GuessProps) {
-    // const wordCounter: { [letter: string]: number } = {};
-    // for (let i = 0; i < word.length; i++) {
-    //     if (!wordCounter[word[i]]) wordCounter[word[i]] = 1;
-    //     else wordCounter[word[i]]++;
-    // }
-
-    // { p: 1, h: 1, o: 2, t: 1, n: 1 }
+    const wordCounter: { [letter: string]: number } = {};
+    for (let i = 0; i < word.length; i++) {
+        if (!wordCounter[word[i]]) wordCounter[word[i]] = 1;
+        else wordCounter[word[i]]++;
+    }
 
     return (
         <div className="flex justify-center gap-2 my-4">
@@ -25,9 +23,11 @@ export default function Guess({ word, guess, isGuessed }: GuessProps) {
                             "flex justify-center items-center h-16 w-16 border-2 font-bold uppercase text-slate-50 text-3xl",
                             !isGuessed
                                 ? "bg-stone-950"
-                                : guess[i] === word[i]
+                                : guess[i] === word[i] &&
+                                  wordCounter[guess[i]]-- > 0
                                 ? "bg-green-600"
-                                : word.includes(guess[i])
+                                : word.includes(guess[i]) &&
+                                  wordCounter[guess[i]]-- > 0
                                 ? "bg-yellow-500"
                                 : "bg-gray-700"
                         )}
